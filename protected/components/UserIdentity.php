4d8b7e782721683f7fda080c0e91bc290f7414ae
Oleg Nagornov <naoi@naoi.notebook>
2013-08-15 16:20:22 +0400

    basic CRUD

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -9,25 +9,27 @@ class UserIdentity extends CUserIdentity
 {
 	/**
 	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
 	 * In practical applications, this should be changed to authenticate
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
+		$user;
+		if (($user = User::model()->findByAttributes(array('username' => $this->username))) === null)
+		{
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
+		}
+		else if (($user = User::model()->findByAttributes(array('username' => $this->username, 'password' => md5($this->password)))) === null)
+		{
+			$this->errorCode=self::ERROR_PASSWORD_INVALID;	
+		}
 		else
+		{
 			$this->errorCode=self::ERROR_NONE;
+			$this->setState('name', $user->username);
+			$this->setState('email', $user->email);
+		}
 		return !$this->errorCode;
 	}
 }
--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,28 +1,222 @@
-CREATE TABLE tbl_user (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(128) NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    email VARCHAR(128) NOT NULL
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-INSERT INTO tbl_user (username, password, email) VALUES ('test1', 'pass1', 'test1@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test2', 'pass2', 'test2@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test3', 'pass3', 'test3@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test4', 'pass4', 'test4@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test5', 'pass5', 'test5@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test6', 'pass6', 'test6@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test7', 'pass7', 'test7@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test8', 'pass8', 'test8@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test9', 'pass9', 'test9@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test10', 'pass10', 'test10@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test11', 'pass11', 'test11@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test12', 'pass12', 'test12@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test13', 'pass13', 'test13@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test14', 'pass14', 'test14@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test15', 'pass15', 'test15@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test16', 'pass16', 'test16@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test17', 'pass17', 'test17@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test18', 'pass18', 'test18@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test19', 'pass19', 'test19@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test20', 'pass20', 'test20@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test21', 'pass21', 'test21@example.com');
+DROP SCHEMA IF EXISTS `library` ;
+CREATE SCHEMA IF NOT EXISTS `library` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `library` ;
+
+-- -----------------------------------------------------
+-- Table `library`.`tbl_book`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `library`.`tbl_book` ;
+
+CREATE  TABLE IF NOT EXISTS `library`.`tbl_book` (
+  `id_book` INT NOT NULL ,
+  `title` VARCHAR(45) NULL ,
+  `description` TEXT NULL ,
+  `current_count` INT NULL ,
+  `total_count` INT NULL ,
+  `file_link` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id_book`) ,
+  UNIQUE INDEX `idBook_UNIQUE` (`id_book` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `library`.`tbl_author`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `library`.`tbl_author` ;
+
+CREATE  TABLE IF NOT EXISTS `library`.`tbl_author` (
+  `id_author` INT NOT NULL ,
+  `name` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id_author`) ,
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `library`.`tbl_type`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `library`.`tbl_type` ;
+
+CREATE  TABLE IF NOT EXISTS `library`.`tbl_type` (
+  `id_type` INT NOT NULL AUTO_INCREMENT ,
+  `type` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id_type`) ,
+  UNIQUE INDEX `idType_UNIQUE` (`id_type` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `library`.`tbl_user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `library`.`tbl_user` ;
+
+CREATE  TABLE IF NOT EXISTS `library`.`tbl_user` (
+  `id_user` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `surname` VARCHAR(45) NOT NULL ,
+  `parentname` VARCHAR(45) NULL ,
+  `email` VARCHAR(45) NOT NULL ,
+  `pass` VARCHAR(45) NOT NULL ,
+  `access` INT NULL ,
+  `check_hash` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id_user`) ,
+  UNIQUE INDEX `idUser_UNIQUE` (`id_user` ASC) ,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `library`.`tbl_query`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `library`.`tbl_query` ;
+
+CREATE  TABLE IF NOT EXISTS `library`.`tbl_query` (
+  `id_query` INT NOT NULL ,
+  `creation_date` TIMESTAMP NULL ,
+  `status` VARCHAR(45) NULL ,
+  `book_id_book` INT NOT NULL ,
+  `user_id_user` INT NOT NULL ,
+  `comment` TEXT NULL ,
+  PRIMARY KEY (`id_query`) ,
+  INDEX `fk_Query_Book1` (`book_id_book` ASC) ,
+  INDEX `fk_Query_User1` (`user_id_user` ASC) ,
+  CONSTRAINT `fk_Query_Book1`
+    FOREIGN KEY (`book_id_book` )
+    REFERENCES `library`.`tbl_book` (`id_book` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Query_User1`
+    FOREIGN KEY (`user_id_user` )
+    REFERENCES `library`.`tbl_user` (`id_user` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `library`.`tbl_recommendation`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `library`.`tbl_recommendation` ;
+
+CREATE  TABLE IF NOT EXISTS `library`.`tbl_recommendation` (
+  `id_recommendation` INT NOT NULL AUTO_INCREMENT ,
+  `reason` VARCHAR(45) NULL ,
+  `book_id_book` INT NOT NULL ,
+  `target_id_user` INT NOT NULL ,
+  `user_id_user` INT NOT NULL ,
+  PRIMARY KEY (`id_recommendation`) ,
+  UNIQUE INDEX `idRecommenadation_UNIQUE` (`id_recommendation` ASC) ,
+  INDEX `fk_Recommenadation_Book1` (`book_id_book` ASC) ,
+  INDEX `fk_Recommenadation_User1` (`target_id_user` ASC) ,
+  INDEX `fk_Recommenadation_User2` (`user_id_user` ASC) ,
+  CONSTRAINT `fk_Recommenadation_Book1`
+    FOREIGN KEY (`book_id_book` )
+    REFERENCES `library`.`tbl_book` (`id_book` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Recommenadation_User1`
+    FOREIGN KEY (`target_id_user` )
+    REFERENCES `library`.`tbl_user` (`id_user` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Recommenadation_User2`
+    FOREIGN KEY (`user_id_user` )
+    REFERENCES `library`.`tbl_user` (`id_user` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `library`.`tbl_keyword`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `library`.`tbl_keyword` ;
+
+CREATE  TABLE IF NOT EXISTS `library`.`tbl_keyword` (
+  `id_keyword` INT NOT NULL AUTO_INCREMENT ,
+  `word` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`id_keyword`) ,
+  UNIQUE INDEX `word_UNIQUE` (`word` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `library`.`tbl_book_has_Author`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `library`.`tbl_book_has_Author` ;
+
+CREATE  TABLE IF NOT EXISTS `library`.`tbl_book_has_Author` (
+  `book_id_book` INT NOT NULL ,
+  `author_id_author` INT NOT NULL ,
+  PRIMARY KEY (`book_id_book`, `author_id_author`) ,
+  INDEX `fk_Book_has_Author_Author1` (`author_id_author` ASC) ,
+  INDEX `fk_Book_has_Author_Book` (`book_id_book` ASC) ,
+  CONSTRAINT `fk_Book_has_Author_Book`
+    FOREIGN KEY (`book_id_book` )
+    REFERENCES `library`.`tbl_book` (`id_book` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Book_has_Author_Author1`
+    FOREIGN KEY (`author_id_author` )
+    REFERENCES `library`.`tbl_author` (`id_author` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `library`.`tbl_book_has_keyword`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `library`.`tbl_book_has_keyword` ;
+
+CREATE  TABLE IF NOT EXISTS `library`.`tbl_book_has_keyword` (
+  `book_id_book` INT NOT NULL ,
+  `key_word_keyword` INT NOT NULL ,
+  PRIMARY KEY (`book_id_book`, `key_word_keyword`) ,
+  INDEX `fk_Book_has_Key_word_Key_word1` (`key_word_keyword` ASC) ,
+  INDEX `fk_Book_has_Key_word_Book1` (`book_id_book` ASC) ,
+  CONSTRAINT `fk_Book_has_Key_word_Book1`
+    FOREIGN KEY (`book_id_book` )
+    REFERENCES `library`.`tbl_book` (`id_book` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Book_has_Key_word_Key_word1`
+    FOREIGN KEY (`key_word_keyword` )
+    REFERENCES `library`.`tbl_keyword` (`id_keyword` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `library`.`tbl_book_has_type`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `library`.`tbl_book_has_type` ;
+
+CREATE  TABLE IF NOT EXISTS `library`.`tbl_book_has_type` (
+  `book_id_book` INT NOT NULL ,
+  `type_id_type` INT NOT NULL ,
+  PRIMARY KEY (`book_id_book`, `type_id_type`) ,
+  INDEX `fk_Book_has_Type_Type1` (`type_id_type` ASC) ,
+  INDEX `fk_Book_has_Type_Book1` (`book_id_book` ASC) ,
+  CONSTRAINT `fk_Book_has_Type_Book1`
+    FOREIGN KEY (`book_id_book` )
+    REFERENCES `library`.`tbl_book` (`id_book` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Book_has_Type_Type1`
+    FOREIGN KEY (`type_id_type` )
+    REFERENCES `library`.`tbl_type` (`id_type` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
