6762fde8465bbad867bdd3df5a9d56cb350f2985
nagornov.oi@gmail.com <nagornov.oi@gmail.com>
2013-08-20 15:53:23 +0400

    statistic widgets

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -27,5 +27,9 @@ if (!Yii::app()->user->isGuest) {
 	$this->widget('application.components.MyRequestsWidget');
 	
 	$this->widget('application.components.MyBooksWidget');
+	
+	$this->widget('application.components.BestBooksWidget');
+	
+	$this->widget('application.components.BestReadersWidget');
 }
 ?>
